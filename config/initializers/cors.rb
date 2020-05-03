312ae177dd84c83000aabf692fc3031154099516
Shinji Okamura <shinji.okamura@newtown-lab.com>
2020-05-03 20:23:23 +0900

    Updated config/initializers/cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins /\Ahttp:\/\/localhost(:\d+)?\z/,
       /\Ahttp:\/\/127\.0\.0\.1(:\d+)?\z/,
-      /\Ahttp:\/\/192\.168\.56\.102(:\d+)?\z/
+      /\Ahttp:\/\/192\.168\.56\.101(:\d+)?\z/
 
     resource '*',
       headers: :any,
