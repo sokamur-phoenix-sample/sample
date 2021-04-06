caf28f1b6f95ec3aadd66184a6cd39156da1d352
Shinji Okamura <shinji.okamura@newtown-lab.com>
2021-04-06 20:52:33 +0900

    Upgraded to Angular v11.2.8

--- frontend/src/app/components/baseball-player-edit/baseball-player-edit.component.spec.ts
+++ frontend/src/app/components/baseball-player-edit/baseball-player-edit.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BaseballPlayerEditComponent } from './baseball-player-edit.component';
 
@@ -6,7 +6,7 @@ describe('BaseballPlayerEditComponent', () => {
   let component: BaseballPlayerEditComponent;
   let fixture: ComponentFixture<BaseballPlayerEditComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BaseballPlayerEditComponent ]
     })
--- frontend/src/app/components/baseball-player-show/baseball-player-show.component.spec.ts
+++ frontend/src/app/components/baseball-player-show/baseball-player-show.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { BaseballPlayerShowComponent } from './baseball-player-show.component';
 
@@ -6,7 +6,7 @@ describe('BaseballPlayerShowComponent', () => {
   let component: BaseballPlayerShowComponent;
   let fixture: ComponentFixture<BaseballPlayerShowComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ BaseballPlayerShowComponent ]
     })
