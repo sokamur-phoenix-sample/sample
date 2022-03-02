3f0ad985dd56d852e8712343fa1a4e0fba70dc36
Shinji Okamura <shinji.okamura@newtown-lab.com>
2022-03-02 13:01:02 +0900

    Updated spec/support/capybara.rb

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,24 +1,21 @@
-# Capybara.register_driver :headless_chrome do |app|
-#   options = Selenium::WebDriver::Chrome::Options.new
-#   options.add_argument('headless')
-#   options.add_argument('disable-gpu')
-#   options.add_argument('no-sandbox')
-#   client = Selenium::WebDriver::Remote::Http::Default.new
-#   client.open_timeout = client.read_timeout = 300
-#   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, http_client: client)
-# end
-
-# Capybara.configure do |config|
-#   config.javascript_driver = :headless_chrome
-#   config.raise_server_errors = false
-# end
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  client = Selenium::WebDriver::Remote::Http::Default.new
+  client.open_timeout = client.read_timeout = 300
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, http_client: client)
+end
 
 RSpec.configure do |config|
   config.before(:each) do |example|
     if example.metadata[:type] == :system
-      driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
-    else
-      driven_by :rack_test
+      if example.metadata[:js]
+        driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
+      else
+        driven_by :rack_test
+      end
     end
   end
 end
