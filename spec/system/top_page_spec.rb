634572726a42daa44fe4e80190b470d4db82dab3
Shinji Okamura <shinji.okamura@newtown-lab.com>
2022-03-02 23:07:29 +0900

    Added system spec

--- spec/system/top_page_spec.rb
+++ spec/system/top_page_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "Top page", type: :system do
   it "displays top page correctly" do
     visit root_path
-    expect(page).to have_content "野球選手一覧"
+    expect(page).to have_content "Loading..."
   end
 end
