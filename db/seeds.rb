8295f609640e55dad4aff40de8414203a817436e
Shinji Okamura <sokamur@newtown-lab.com>
2023-08-29 10:55:18 +0900

    Updated db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-BaseballPlayer.create!(name:'坂本 勇人', team:'読売ジャイアンツ', position:'内野手', hometown:'兵庫県')
-BaseballPlayer.create!(name:'鳥谷 敬', team:'阪神タイガース', position:'内野手', hometown:'東京都')
-BaseballPlayer.create!(name:'菅野 智之', team:'読売ジャイアンツ', position:'投手', hometown:'神奈川県')
-BaseballPlayer.create!(name:'筒香 嘉智', team:'横浜ＤｅＮＡベイスターズ', position:'内野手', hometown:'和歌山県')
-BaseballPlayer.create!(name:'マイコラス', team:'読売ジャイアンツ', position:'投手', hometown:'アメリカ')
-BaseballPlayer.create!(name:'松井 裕樹', team:'東北楽天ゴールデンイーグルス', position:'投手', hometown:'神奈川県')
+BaseballPlayer.create!(name:'大谷 翔平', team:'ロサンゼルス・エンジェルス', position:'投手兼DH', hometown:'岩手県')
+BaseballPlayer.create!(name:'岡本 和真', team:'読売ジャイアンツ', position:'一塁手', hometown:'奈良県')
+BaseballPlayer.create!(name:'佐々木 朗希', team:'千葉ロッテマリーンズ', position:'投手', hometown:'岩手県')
 Schedule.create!(title: "スケジュール", all_day: true, start: Time.zone.now, end: Time.zone.now + 30.minutes, background_color: "orange", border_color: "blue", text_color: "blue", user_id: 1, contractor_id: 1)
