f01bb2301792cf77ce382a540f4c14a2392b734c
Shinji Okamura <sokamur@newtown-lab>
2024-02-28 16:28:37 +0900

    Added spec/requests/baseball_players_spec

--- spec/requests/baseball_players_spec.rb
+++ spec/requests/baseball_players_spec.rb
@@ -1,13 +1,22 @@
-require 'rails_helper'
+require "swagger_helper"
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "BaseballPlayers", type: :request do
   describe "GET api/v1/baseball_players" do
-    before do
-      get api_v1_baseball_players_path
-    end
+    path "/api/v1/baseball_players" do
+      get "GET baseball_players" do
+        consumes "application/json"
+        produces "application/json"
+        response 200, "Success" do
+          # before do
+          #   get api_v1_baseball_players_path
+          # end
+          schema expected_response_schema.schema.as_json
 
-    it "succeeds to responde with success" do
-      expect(response).to have_http_status(:success)
+          it "succeeds to responde with success" do
+            expect(response).to have_http_status(:success)
+          end
+        end
+      end
     end
   end
 end
