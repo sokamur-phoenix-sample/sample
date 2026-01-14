a48ca9b63012ff88d366f9a10023d41543301eff
Shinji Okamura <sokamur@newtown-lab>
2026-01-14 17:49:01 +0900

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2022_02_20_134729) do
+ActiveRecord::Schema[8.1].define(version: 2022_02_20_134729) do
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
+    t.string "content_type"
     t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -40,26 +40,26 @@ ActiveRecord::Schema[8.0].define(version: 2022_02_20_134729) do
   end
 
   create_table "baseball_players", force: :cascade do |t|
-    t.string "name"
-    t.string "team"
-    t.string "position"
-    t.string "hometown"
     t.datetime "created_at", precision: nil, null: false
+    t.string "hometown"
+    t.string "name"
+    t.string "position"
+    t.string "team"
     t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "schedules", force: :cascade do |t|
-    t.string "title"
     t.boolean "all_day"
-    t.datetime "start", precision: nil
-    t.datetime "end", precision: nil
     t.string "background_color"
     t.string "border_color"
-    t.string "text_color"
-    t.integer "user_id"
     t.integer "contractor_id"
     t.datetime "created_at", precision: nil, null: false
+    t.datetime "end", precision: nil
+    t.datetime "start", precision: nil
+    t.string "text_color"
+    t.string "title"
     t.datetime "updated_at", precision: nil, null: false
+    t.integer "user_id"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
