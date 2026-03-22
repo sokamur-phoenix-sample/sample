048e095f0591ca295126e8354c7cf76d81333153
Shinji Okamura <sokamur@newtown-lab>
2026-03-22 23:09:42 +0900

    Updated .circleci/config.yml

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 BaseballPlayer.create!(name:'大谷 翔平', team:'ロサンゼルス・エンジェルス', position:'投手兼DH', hometown:'岩手県')
-BaseballPlayer.create!(name:'岡本 和真', team:'読売ジャイアンツ', position:'一塁手', hometown:'奈良県')
-BaseballPlayer.create!(name:'佐々木 朗希', team:'千葉ロッテマリーンズ', position:'投手', hometown:'岩手県')
+BaseballPlayer.create!(name:'岡本 和真', team:'トロント・ブルージェイズ', position:'一塁手', hometown:'奈良県')
+BaseballPlayer.create!(name:'佐々木 朗希', team:'ロサンゼルス・エンジェルス', position:'投手', hometown:'岩手県')
 Schedule.create!(title: "スケジュール", all_day: true, start: Time.zone.now, end: Time.zone.now + 30.minutes, background_color: "orange", border_color: "blue", text_color: "blue", user_id: 1, contractor_id: 1)
