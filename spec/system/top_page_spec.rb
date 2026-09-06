2229b04cc509676bb4bafc9086268aef0b672e89
Shinji Okamura <sokamur@newtown-lab>
2026-09-06 15:26:43 +0900

    Updated spec/system/top_page_spec.rb

--- spec/system/top_page_spec.rb
+++ spec/system/top_page_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe "Top page", type: :system do
   it "displays top page correctly" do
     visit root_path
-    expect(page).to have_link "野球選手一覧", href: "/baseball-players"
+    expect(page).to have_content "Loading..."
   end
 end
